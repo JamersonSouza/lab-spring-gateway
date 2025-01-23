cf3c54f6179a979eb57c8c96a96446bb40e14cd0
jamersonsouza <jamersonsouza@outlook.com>
2025-01-22 22:00:33 -0300

    ✨ feat:  ajustes nos endpoints de retorno dos clients

--- client-api-alternative/src/main/java/tech/jamersondev/client_api_alternative/controllers/ClientController.java
+++ client-api-alternative/src/main/java/tech/jamersondev/client_api_alternative/controllers/ClientController.java
@@ -1,16 +1,19 @@
 package tech.jamersondev.client_api_alternative.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/clients")
 public class ClientController {
     @GetMapping
-    public void listClients(){
-        Arrays.asList("Jamerson", "Nathali", "Marinho").stream().forEach(System.out::println);
+    public ResponseEntity<List<String>> listClients(){
+        List<String> clients = Arrays.asList("Jamerson", "Nathali", "Marinho");
+        return ResponseEntity.ok(clients);
     }
 }
--- client-api/src/main/java/tech/jamersondev/client_api/controllers/ClientController.java
+++ client-api/src/main/java/tech/jamersondev/client_api/controllers/ClientController.java
@@ -1,17 +1,20 @@
 package tech.jamersondev.client_api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
 
     @GetMapping
-    public void listClients(){
-        Arrays.asList("Jamerson", "Tathyane", "Luiza").stream().forEach(System.out::println);
+    public ResponseEntity<List<String>> listClients(){
+        List<String> clients = Arrays.asList("Jamerson", "Luiza", "Edson");
+        return ResponseEntity.ok(clients);
     }
 }
