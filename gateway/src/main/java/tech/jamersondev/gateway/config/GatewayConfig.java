a64c5f092d2e3eb40495bc9e9da0a291f5c60a42
jamersonsouza <jamersonsouza@outlook.com>
2025-01-27 21:54:55 -0300

    feat: config de um filter para fallback

--- gateway/src/main/java/tech/jamersondev/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/tech/jamersondev/gateway/config/GatewayConfig.java
@@ -4,17 +4,41 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import tech.jamersondev.gateway.service.FallbackService;
+
 
 @Configuration
 public class GatewayConfig {
 
+    private final FallbackService fallbackService;
+
+    public GatewayConfig(@Lazy FallbackService fallbackService) {
+        this.fallbackService = fallbackService;
+    }
+
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
         return routeLocatorBuilder.routes()
-                .route("client", c -> c
+                .route("client", route -> route
                         .path("/clients")
-                        .uri("http://localhost:8089"))
+                        .filters(f -> f
+                                .retry(config -> config
+                                        .setRetries(3)
+                                        .setStatuses(
+                                                HttpStatus.INTERNAL_SERVER_ERROR,
+                                                HttpStatus.BAD_REQUEST,
+                                                HttpStatus.SERVICE_UNAVAILABLE)
+                                )
+                                .filter((exchange, chain) -> chain.filter(exchange)
+                                        .onErrorResume(throwable -> fallbackService.handleFallback(exchange)))
+                        )
+                        .uri("http://localhost:8089")
+                )
                 .build();
     }
+
+
 
 }
